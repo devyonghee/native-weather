5cd521a158e8824d7c24b9e133d7dfc59d411a5b
yhhan <shiw122333@gmail.com>
2019-02-13 00:07:53 +0900

    error handle

--- App.js
+++ App.js
@@ -4,17 +4,28 @@ import Weather from './Weather';
 
 export default class App extends Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null
   };
+  componentDidMount() { 
+      navigator.geolocation.getCurrentPosition(position => {
+          this.setState({error:"someting"})
+        },
+        error => {
+          this.setState({error:error})
+        }
+      )
+  }
   
   render() {
-    const {isLoaded} = this.state;
+    const {isLoaded, error} = this.state;
     return (
       <View style={styles.container}>
       <StatusBar hidden={true}/>
         {isLoaded ? <Weather/> : 
           (<View style={styles.loading}>
              <Text style={styles.loadingText}>Getting the fucking weather</Text>
+             {error ? <Text style={styles.errorText}>{error}</Text>:null}
           </View>)
         }
       </View>
@@ -26,6 +37,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  errorText:{
+color:"red",
+backgroundColor:"transparent"
   },
   loading:{
     flex:1,

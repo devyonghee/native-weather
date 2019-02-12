ed73a17806e25db7db4a829d640906744c489ead
yhhan <shiw122333@gmail.com>
2019-02-13 00:36:04 +0900

    get weather from open weather map api

--- App.js
+++ App.js
@@ -2,20 +2,48 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from './Weather';
 
+const API_KEY = "ab9c0c8c00a396595fe9e690d55b8f9d";
+
 export default class App extends Component {
   state = {
     isLoaded: false,
+<<<<<<< HEAD
     error: null
   };
   componentDidMount() { 
       navigator.geolocation.getCurrentPosition(position => {
           this.setState({error:"someting"})
+=======
+    error: null,
+    temperature:null,
+    name:null
+  };
+  componentDidMount() { 
+      navigator.geolocation.getCurrentPosition(position => {
+          this._getWeather(position.coords.latitude, position.coords.longitude)
+>>>>>>> get weather data by open weather map api
         },
         error => {
           this.setState({error:error})
         }
       )
   }
+<<<<<<< HEAD
+=======
+
+  _getWeather(lat, long) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature:json.main.temp,
+        name:json.weather[0].main,
+        isLoaded:true
+      })
+    })
+  }
+>>>>>>> get weather data by open weather map api
   
   render() {
     const {isLoaded, error} = this.state;
---
+++ README.md
@@ -0,0 +1,1 @@
+open weather map 사이트에 가입해서 api를 통해 날씨정보를 가져옴

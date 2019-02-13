e7d8a3608277e6a727a70df077110fd6ed0b7c75
yhhan <shiw122333@gmail.com>
2019-02-13 23:44:52 +0900

    finished lecture

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import Weather from './Weather';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
 
 const API_KEY = "ab9c0c8c00a396595fe9e690d55b8f9d";
 
@@ -8,43 +8,51 @@ export default class App extends Component {
   state = {
     isLoaded: false,
     error: null,
-    temperature:null,
-    name:null
+    temperature: null,
+    name: null
   };
-  componentDidMount() { 
-      navigator.geolocation.getCurrentPosition(position => {
-          this._getWeather(position.coords.latitude, position.coords.longitude)
-        },
-        error => {
-          this.setState({error:error})
-        }
-      )
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({ error: error });
+      }
+    );
   }
 
   _getWeather(lat, long) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-      this.setState({
-        temperature:json.main.temp,
-        name:json.weather[0].main,
-        isLoaded:true
-      })
-    })
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
   }
-  
+
   render() {
-    const {isLoaded, error} = this.state;
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
-      <StatusBar hidden={true}/>
-        {isLoaded ? <Weather/> : 
-          (<View style={styles.loading}>
-             <Text style={styles.loadingText}>Getting the fucking weather</Text>
-             {error ? <Text style={styles.errorText}>{error}</Text>:null}
-          </View>)
-        }
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather
+            weatherName={"Haze"}
+            temp={Math.ceil(temperature - 273.15)}
+          />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
       </View>
     );
   }
@@ -53,20 +61,20 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
-  errorText:{
-color:"red",
-backgroundColor:"transparent"
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent"
   },
-  loading:{
-    flex:1,
-    backgroundColor:'#FDF6AA',
-    justifyContent:'flex-end',
-    paddingLeft:25,
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
   },
-  loadingText:{
-    fontSize:38,
-    marginBottom:100
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
   }
 });
--- Weather.js
+++ Weather.js
@@ -1,59 +1,125 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {LinearGradient} from 'expo';
-import {Ionicons} from "@expo/vector-icons";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class Weather extends Component {
-    render(){
-        return (
-            <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
-                <View style={styles.upper}>
-                    <Ionicons color="white" size={144} name="ios-rainy"/>
-                    <Text style={styles.temp}>35도</Text>
-                </View>
-                <View style={styles.lower}>
-                <Text style={styles.title}> Raining like a MF</Text>
-                <Text style={styles.subtitle}> For moe info loo outsie</Text>
-                </View>
-            </LinearGradient>
-        );
-    }
+const weatherCases = {
+  Rain: {
+    colors: ["#00C6FB", "#005BEA"],
+    title: "Raining like a MF",
+    subtitle: "For more info look outside",
+    icon: "weather-rainy"
+  },
+  Clear: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt",
+    icon: "weather-sunny"
+  },
+  Thunderstorm: {
+    colors: ["#00ECBC", "#007ADF"],
+    title: "thunderstorm in the house",
+    subtitle: "Actually, outside of the house",
+    icon: "weather-lightning"
+  },
+  Clouds: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Clouds",
+    subtitle: "I know, funcking boring",
+    icon: "weather-cloudy"
+  },
+  snow: {
+    colors: ["#7DE2FC", "#B9B6E5"],
+    title: "Cold as balls",
+    subtitle: "Do you want to build a snowman?",
+    icon: "weather-snowy"
+  },
+  Drizzle: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Drizzle",
+    subtitle: "Is like rain",
+    icon: "weather-hail"
+  },
+  Haze: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Haze",
+    subtitle: "Don`t know what that is it",
+    icon: "weather-hail"
+  },
+  Mist: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Mist!",
+    subtitle: "Don`t know what that is it",
+    icon: "weather-fog"
+  }
+};
+
+function Weather({ temp, weatherName }) {
+  console.log(weatherName);
+  return (
+    <LinearGradient
+      colors={weatherCases[weatherName].colors}
+      style={styles.container}
+    >
+      <View style={styles.upper}>
+        <MaterialCommunityIcons
+          color="white"
+          size={144}
+          name={weatherCases[weatherName].icon}
+        />
+        <Text style={styles.temp}>{temp}</Text>
+      </View>
+      <View style={styles.lower}>
+        <Text style={styles.title}> {weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherCases[weatherName].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1
-    },
-    upper:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center",
-        backgroundColor:"transparent",
-    },
-    temp:{        
-        fontSize:48,
-        backgroundColor:"transparent",
-        color:"white",
-        marginTop:10
-    },
-    lower:{
-        flex:1,
-        alignItems:"flex-start",
-        justifyContent:"flex-end",
-        paddingLeft:25,
-        fontWeight:"300"
-    },
-    title:{
-        fontSize:38,
-        backgroundColor:"transparent",
-        color:"white",
-        marginBottom:10
-    },
-    subtitle:{
-        fontSize:24,
-        backgroundColor:"transparent",
-        color:"white",
-        marginBottom:24
-    }
+export default Weather;
 
-})
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  weatherName: PropTypes.string.isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent"
+  },
+  temp: {
+    fontSize: 48,
+    backgroundColor: "transparent",
+    color: "white",
+    marginTop: 10
+  },
+  lower: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    paddingLeft: 25,
+    fontWeight: "300"
+  },
+  title: {
+    fontSize: 38,
+    backgroundColor: "transparent",
+    color: "white",
+    marginBottom: 10
+  },
+  subtitle: {
+    fontSize: 24,
+    backgroundColor: "transparent",
+    color: "white",
+    marginBottom: 24
+  }
+});
